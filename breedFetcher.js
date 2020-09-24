8f605b48281f03435c9f06d9b5b4b2300fbeb25d
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-24 19:10:49 +0000

    final refactor and cleaning up, + testing mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
-const breedChoice = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedChoice}`;
 
-const get = function(url,callback) {
+const fetchBreedDescription = function(breedChoice,callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedChoice}`;
   request.get(url,function(err,resp,body) {
     if (err) {
-      throw "error getting URL" + err;
-
+      callback(err,null)
+    }  else if (!breedChoice) {
+      callback(err,null)
     } else {
       const data = JSON.parse(body);
       if (data.length === 0) {
-        throw "Breed not found.";
+        callback(err,null)
+      } else {
+        callback(null,data[0].description);
       }
-      callback(data);
+       
     }
   });
 };
-get (url, (elem) => {
-  console.log(elem);
-});
+
+
+module.exports = {fetchBreedDescription};
